4695f04f3f2ba2e201649f2bbdf23a168ebfd5f5
Nik <nikeshpatel356@gmail.com>
2019-02-13 21:46:43 -0500

    Solved jump issue and added camera controller

---
+++ Week#2-InClass/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform playerPosition;
+    public Transform moveThresold;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerPosition.position.x > moveThresold.position.x)
+        {
+            //move camere private void OnApplicationPause(bool pauseStatus) {
+            transform.position = new Vector3(playerPosition.position.x, transform.position.y, transform.position.z);
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(moveThresold.position, new Vector2(moveThresold.position.x, moveThresold.position.y + 10));
+        Gizmos.DrawLine(moveThresold.position, new Vector2(moveThresold.position.x, moveThresold.position.y - 10));
+    }
+
+}
--- Week#2-InClass/Assets/Scripts/PlayerController.cs
+++ Week#2-InClass/Assets/Scripts/PlayerController.cs
@@ -6,27 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     // PUBLIC VARIABLES
     public float speed = 10.0f;
-    public float jumpForce = 500.0f;
+    public float jumpForce = 100.0f;
 
     // PRIVATE VARIABLES
     private Rigidbody2D rBody;
-    private bool canJump = false;
+    private Animator anim ;
+    private bool isFacingRight = true;
+    private bool isGrounded = false;
+    //private bool canJump = false;
 
     // Reserved function. Runs only once when the object is created.
     // Used for initialization
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) // Listens to my space bar key being pressed
-        {
-            rBody.AddForce(new Vector2(0, jumpForce));
-            canJump = false;
-        }
+        
     }
+
+     /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    
 
     /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
@@ -36,6 +43,43 @@ public class PlayerController : MonoBehaviour
     {
         float horiz = Input.GetAxis("Horizontal");
 
+        // Listens to my space bar key being pressed
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rBody.AddForce(new Vector2(0, jumpForce));
+            isGrounded = false;
+            //canJump = false;
+        }
+
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        //check direction of player
+        if(horiz < 0 && isFacingRight)
+        {
+            Flip();
+        }
+        else if(horiz > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        //Update animator information
+        anim.SetFloat("Speed", Mathf.Abs(horiz));
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+      if(other.gameObject.CompareTag("Ground"))
+      {
+          isGrounded = true;
+      }   
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
     }
 }
